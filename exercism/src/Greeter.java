a8453fb09d4874a192ac70078a2fee8a5dd09df8
GregSimpson <gsimpson@pobox.com>
2020-02-03 13:57:23 -0700

    hello world in intellij

--- exercism/src/Greeter.java
+++
@@ -1,8 +0,0 @@
-public class Greeter {
-    String getGreeting() {
-        //System.out.println("Hello, World!");
-        String hi = "Hello, World!";
-        return hi;
-        //throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-}
---
+++ hw_three/src/com/example/Hw_three.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Hw_three {
+    public static void main(String[] args) {
+        System.out.println("java_module Hw_three message");
+    }
+}
