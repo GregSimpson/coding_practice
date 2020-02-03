d663dcf45c08922524647c76698856796b96a0a9
GregSimpson <gsimpson@pobox.com>
2020-02-03 12:13:00 -0700

    hello world in intellij

--- exercism/src/Greeter.java
+++ exercism/src/Greeter.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Greeter {
+    String getGreeting() {
+        //System.out.println("Hello, World!");
+        String hi = "Hello, World!";
+        return hi;
+        //throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
 }
